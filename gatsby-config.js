bbc2f887e867268f5d203522656290da463c4f6a
craigmcg <craigmcgowan15@gmail.com>
2022-02-02 20:23:11 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMetadata: {
+      title: `cmdev-portfolio`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "lICae9IgVhvIDrk75maeZsRxD4qqtMYL8hA-8w0P8M4",
+      "spaceId": "a9aj5bcg0qv1"
+    }
+  }, "gatsby-plugin-styled-components"]
+};
