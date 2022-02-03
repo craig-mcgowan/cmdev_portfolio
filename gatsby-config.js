49dc8b5f510b7baa3370543b41ba627d184ca5b2
craigmcg <craigmcgowan15@gmail.com>
2022-02-02 21:12:16 -0500

    installs gatsby-plugin-layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,5 +9,6 @@ module.exports = {
       "accessToken": "lICae9IgVhvIDrk75maeZsRxD4qqtMYL8hA-8w0P8M4",
       "spaceId": "a9aj5bcg0qv1"
     }
-  }, "gatsby-plugin-styled-components"]
+  }, "gatsby-plugin-styled-components",
+    `gatsby-plugin-layout`]
 };
