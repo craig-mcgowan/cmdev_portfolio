e9695ffa856284d2984b0ca19dcd269d1d9e6dc9
craigmcg <craigmcgowan15@gmail.com>
2022-02-02 21:31:25 -0500

    adds new plugins to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,19 @@
 module.exports = {
   siteMetadata: {
-      title: `cmdev-portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `cmdev-portfolio`,
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "lICae9IgVhvIDrk75maeZsRxD4qqtMYL8hA-8w0P8M4",
-      "spaceId": "a9aj5bcg0qv1"
-    }
-  }, "gatsby-plugin-styled-components",
-    `gatsby-plugin-layout`]
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "lICae9IgVhvIDrk75maeZsRxD4qqtMYL8hA-8w0P8M4",
+        spaceId: "a9aj5bcg0qv1",
+      },
+    },
+    "gatsby-plugin-styled-components",
+    `gatsby-plugin-layout`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`
+  ],
 };
