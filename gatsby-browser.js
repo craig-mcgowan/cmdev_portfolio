33f971a4ab708fff1e62b14f98b2d4f62ed4c3fd
craigmcg <craigmcgowan15@gmail.com>
2022-02-08 23:27:49 -0500

    header styling/animation

---
+++ gatsby-browser.js
@@ -0,0 +1,2 @@
+import "./styles.css"
+import "@fontsource/oxygen"
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,15 @@
 import React from "react"
 import { Main } from "../styled/Main"
 import Header from "./header"
+import * as mainStyle from "../components/main.module.scss"
 
 export default function Layout({ children }) {
   return (
     <>
       <Header />
-      <Main>
+      <main>
         {children}
-      </Main>
+      </main>
 
     </>
   )
